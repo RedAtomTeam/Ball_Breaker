844b6e3e3d87bba839148faab5f8f877a6935b46
Genchik <redatomteam@gmail.com>
2025-07-17 13:05:04 +0300

    end

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    List<int> LevelsDamage = new List<int>();
+
+    private void Awake()
+    {
+        
+    }
+
+
+}
